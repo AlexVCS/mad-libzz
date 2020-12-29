ae68be15b0ef42c00ad61eb5b9f1d9d5b8134beb
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-12-29 10:18:39 -0500

    Changed useEffect function to get a response from the API

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
-function GetApiData() {
-useEffect(() => {
-async function fetchData() {
-  const res = await fetch('http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=15');
- }
- fetchData();
-}, []);
-}
-
-console.log(GetApiData);
-console.log(res);
-
 function App() {
+  useEffect(async () => {
+    const res = await fetch('http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=15%27');
+    console.log(res);
+  }, []);
+
   return (
     <div className="App">
+      {/* {res.map(field =>
+        <div key={}>
+          <span></span>
+        </div>
+      )} */}
       Bonjour
     </div>
   );
