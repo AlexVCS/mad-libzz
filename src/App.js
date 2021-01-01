d360a419cf757a120172681dbb0e7f6224d9d107
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2020-12-31 19:22:25 -0500

    Working with the response from the API

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [ fields, setfields ] = useState([]);
+  const [ fields, setFields ] = useState([])
 
   useEffect(async () => {
     const res = await fetch('http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=15%27');
     console.log(res);
-    // setFields(res.headers);
+    // console.log(res.headers);
   }, []);
-  
-
-  
 
   return (
     <div className="App">
       {fields.map(field =>
         <div key={fields.id}>
           <span>
-            <input placeholder="Add your here" type="text"/>
+          <input placeholder="Add your here" type="text"/>
           </span>
         </div>
-      )}
+        )}
       Bonjour
     </div>
   );
