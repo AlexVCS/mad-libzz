9aef2c3cca80391baab027df7b32a80a1a1b6833
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-01-01 18:43:42 -0500

    refactored useEffect and blanks.map function, changed names of some of the state variables

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const [ fields, setFields ] = useState([])
+  const [ blanks, setBlanks ] = useState([])
+  const [ values, setValues ] = useState([])
+  const [ newValues, setNewValues ] = useState([])
 
   useEffect(async () => {
     const res = await fetch('http://madlibz.herokuapp.com/api/random?minlength=5&maxlength=15%27');
-    console.log(res);
-    // console.log(res.headers);
+    const data = await res.json();
+    console.log(data);
+    setBlanks(data.blanks)
+    setValues(data.value)
   }, []);
+
+  const handleValueChange = (event) => {
+    console.log(setNewValues(event.target.value))
+  }
 
   return (
     <div className="App">
-      {fields.map(field =>
-        <div key={fields.id}>
+      {blanks.map((blank, i) =>
+        <div key={i}>
+          <div>
+          {blank}
+          </div>
           <span>
-          <input placeholder="Add your here" type="text"/>
+          <input onChange={handleValueChange} placeholder={`Enter ${blank}`} type="text"/>
           </span>
         </div>
         )}
-      Bonjour
     </div>
   );
 }
 
 export default App;
+
+// store onChange in state
+// third state called values, track everything that's changed in the input and store it in the state
