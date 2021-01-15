400fa63ef8c880f27ddcdee4e7df7b2a4a9e7011
Alex Curtis-Slep <alexcurtisslep@gmail.com>
2021-01-14 21:12:29 -0500

    Added line 17 to enable new game to create a new game. Indentation changes

--- src/App.js
+++ src/App.js
@@ -11,10 +11,10 @@ function App() {
   async function fetchData() {
     const res = await fetch('https://madlibz.herokuapp.com/api/random?minlength=5&maxlength=15%27');
     const data = await res.json();
-    console.log(data);
     setBlanks(data.blanks)
     setValues(data.value)
     setTitle(data.title)
+    setStory([])
     }
 
     useEffect(() => {
@@ -38,34 +38,35 @@ function App() {
 
   return (
     <div className="app">
-    <div className="header">Madlibzz Game</div>
-      {!story.length && blanks.map((blank, index) =>
-        <div className="input-group" key={index}>
-          <div className="wordTypeText">
-          {blank}
-          </div>
-          <span>
-          <input onChange={(event) => {handleValueChange(event, index)}} placeholder={`Enter ${blank}`} className="story-blank" type="text"/>
-          </span>
-        </div>
-      )}
-      {!!story.length && <> <div className="story-title">
-      {title}</div>
-      <div className="story-text">{story}
-      </div>
-      </>}
+      <div className="header">Madlibzz Game</div>
+        {!story.length && blanks.map((blank, index) =>
+          <div className="input-group" key={index}>
+            <div className="wordTypeText">
+              {blank}
+            </div>
+            <span>
+            <input onChange={(event) => {handleValueChange(event, index)}} placeholder={`Enter ${blank}`} className="story-blank" type="text"/>
+            </span>
+            </div>
+            )}
+            {!!story.length &&
+            <>
+            <div className="story-title">
+            {title}
+            </div>
+            <div className="story-text">{story}
+            </div>
+            </>}
      
-      { story.length === 0
-        ? <div onClick={onSubmit} className="submitAndNewGameButton">Submit</div>
-        : <div onClick={fetchData} className="submitAndNewGameButton">New Game</div>
-      }
-    </div>
-  );
+            { story.length === 0
+              ? <div onClick={onSubmit} className="submitAndNewGameButton">Submit</div>
+              : <div onClick={fetchData} className="submitAndNewGameButton">New Game</div>
+              }
+              </div>
+              );
 }
 
 export default App;
-
-// add functionality to new game button that allows a new game to start
 
 // add validation if the button is clicked but not all inputs have text in them
 // alert('Please fill in all the blanks!')
